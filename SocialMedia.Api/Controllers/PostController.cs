a4f5ab19c75c01035ceb681b33c7d296ab1fedac
Angel Gabriel <angelgabrielda@hotmail.com>
2020-10-18 21:33:49 -0500

    Part 5 - HTTP Basics and REST

--- SocialMedia.Api/Controllers/PostController.cs
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.Entities;
 using SocialMedia.Core.Interfaces;
 using SocialMedia.Infrastructure.Repositories;
 using System.Threading.Tasks;
@@ -32,6 +33,13 @@ namespace SocialMedia.Api.Controllers
             var post = await _postRepository.GetPost(id);
             return Ok(post);
         }
-        
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Post post)
+        {
+            await _postRepository.InsertPost(post);
+            return Ok(post);
+        }
+
     }
 }
